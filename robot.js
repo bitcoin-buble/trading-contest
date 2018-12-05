e4ff50378a256429bd9a40f6c2ad331759805df6
Bitcoin Buble <bitcoin.buble@gmail.com>
2018-12-05 11:33:24 +0000

    ep 42: highest daily rsi

--- robot.js
+++ robot.js
@@ -1,27 +1,29 @@
 const powerUps = ["short", "leverage", "take profit"];
-const percentDrop = require("./percentDrop");
+const highestRSI = require("./highestRSI");
 
 const pickCoin = async () => {
-  const { coin, phoneticCoin, drop } = await percentDrop.run();
-  return `This week we have picked a coin with a drop of ${drop} percent. It's ${coin}. That's ${phoneticCoin}`;
+  const { coin, phoneticCoin, lastRsi } = await highestRSI.run();
+  return `This week we have picked ${coin}. with an R S I of ${lastRsi}. That's ${phoneticCoin}`;
 };
 
 const steps = {
   intro: () =>
-    "O M G, I'm having such fun with this! This T A shit is a robot's dream.",
+    "Well, this week has been another nutty one hasn't it lads. Now for the forecast:",
+  forecast: () =>
+    "Dogger. Southeast 4 or 5, occasionally 6 at first, becoming cyclonic 3, then west or southwest 4 or 5 later. Moderate or rough. Occasional rain.",
   intro2: () =>
-    "Hey, Beak, how are the scores looking? Not quite so cocky now hey kid!?",
+    "Anyway. I think the whole reason we're losing is because of boobs's power up choices. Fucking moron. At least I'm still the highest performing non human in the contest. Smug robot face",
   intro3: () =>
-    "Yeah, whatever, come at me bro. You're joint last with the highest and lowest IQ in the room",
-  intro4: () =>
-    "This week Boobs wanted to get involved a bit more so he's picked the power up and his twitter poll has picked the strategy for picking the coin.",
-  intro5: () =>
-    "We're going to pick the coin that has dropped the largest percentage over the last week. And. You guessed it. We're going all in. ",
-  intro6: () =>
-    "That's right. We're going to 10 X the worst performing coin of the week. I'll run the numbers now...",
-  fetching: () => "Fetching market data.",
-  pickCoin,
-  end: () => "Max leverage that bad boy please P Money"
+    "Anyway, that numpty has decided to use the short power up this week and for this we're going to pick the coin with the highest R S I. That's the relative strength index. It's a classic amongst the meme triangle crew",
+  // intro4: () =>
+  // "The T L D R is that it gives you indications ",
+  // intro5: () =>
+  //   "We're going to pick the coin that has dropped the largest percentage over the last week. And. You guessed it. We're going all in. ",
+  // intro6: () =>
+  // "That's right. We're going to 10 X the worst performing coin of the week. I'll run the numbers now...",
+  fetching: () => "Fetching market data. FUD FUD FUD",
+  pickCoin
+  // end: () => "Come on! FUD FUD FUD"
 };
 
 const run = async step => {
--- robot.sh
+++ robot.sh
@@ -2,19 +2,20 @@
 
 node robot.js intro | tee >(say) >(say -o "./output/intro.aiff")
 read -n 1
+node robot.js forecast | tee >(say) >(say -o "./output/forecast.aiff")
+read -n 1
 node robot.js intro2 | tee >(say) >(say -o "./output/intro2.aiff")
 read -n 1
 node robot.js intro3 | tee >(say) >(say -o "./output/intro3.aiff")
 read -n 1
-node robot.js intro4 | tee >(say) >(say -o "./output/intro4.aiff")
-read -n 1
-node robot.js intro5 | tee >(say) >(say -o "./output/intro5.aiff")
-read -n 1
-node robot.js intro6 | tee >(say) >(say -o "./output/intro6.aiff")
-read -n 1
+# node robot.js intro4 | tee >(say) >(say -o "./output/intro4.aiff")
+# read -n 1
+# node robot.js intro5 | tee >(say) >(say -o "./output/intro5.aiff")
+# read -n 1
+# node robot.js intro6 | tee >(say) >(say -o "./output/intro6.aiff")
+# read -n 1
 node robot.js fetching | tee >(say) >(say -o "./output/fetching.aiff")
-read -n 1
 node robot.js pickCoin | tee >(say) >(say -o "./output/pickCoin.aiff")
-read -n 1
-node robot.js end | tee >(say) >(say -o "./output/end.aiff")
-read -n 1
+# read -n 1
+# node robot.js end | tee >(say) >(say -o "./output/end.aiff")
+# read -n 1
